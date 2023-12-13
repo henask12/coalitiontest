259b52d82e18d14445e6973ca15cee878d3666c3
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-12-13 08:26:20 +0300

    update app js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Team from './components/Team';
+import History from './components/History';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Home/>
+     <main className="overflow-hidden">
+     <Navbar backgroundColor="white" linkColor="black" header />
+     <History/>
+     <Team/>
+     </main>
+     <Footer/>
+
     </div>
   );
 }
